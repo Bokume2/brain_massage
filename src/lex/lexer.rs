6749d39e9663147622f51fbe392c3c3b1176e717
Bokume2 <nagatomoyuu2@gmail.com>
2026-05-11 18:08:23 +0900

    update: 正規表現のコンパイルを一度だけ行うように

--- src/lex/lexer.rs
+++ src/lex/lexer.rs
@@ -1,99 +1,146 @@
+use std::sync::LazyLock;
+
 use anyhow::{Result, bail};
 use regex::Regex;
 
 use crate::lex::{AssginToken, BracketToken, CurlyBracketToken, Token, VariableToken};
 
+pub const PUT_KW: &str = "put";
+pub const GET_KW: &str = "get";
+pub const WHILE_KW: &str = "while";
+pub const HEAD_KW: &str = "head";
+
+pub const SEMICOLON: &str = ";";
+pub const LBRACKET: &str = "(";
+pub const RBRACKET: &str = ")";
+pub const LCBRACKET: &str = "{";
+pub const RCBRACKET: &str = "}";
+
+pub const SIMPLE_ASSIGN_OP: &str = "=";
+pub const ADD_ASSIGN_OP: &str = "+=";
+pub const SUB_ASSIGN_OP: &str = "-=";
+
+pub const VAR_PREFIX: &str = "v";
+
+const NUMBER_PTN: &str = r"(?i)(0x[0-9A-F]+)|([0-9]+)";
+const DELIMITER_PTN: &str = r"[ \t\r\n;\(\)\{\}]+";
+
 pub struct Lexer;
 
 impl Lexer {
     pub fn lex(&self, code: &str) -> Result<Vec<Token>> {
-        let put_kw = "put";
-        let get_kw = "get";
-        let while_kw = "while";
-        let head_kw = "head";
-        let semicolon = ";";
-        let lbracket = "(";
-        let rbracket = ")";
-        let bracket_ptn = &format!(r"[\{}\{}]", lbracket, rbracket);
-        let lcbracket = "{";
-        let rcbracket = "}";
-        let curly_bracket_ptn = &format!(r"[\{}\{}]", lcbracket, rcbracket);
-        let simple_assign_op = "=";
-        let add_assign_op = "+=";
-        let sub_assign_op = "-=";
-        let assign_ptn = &format!(
-            r"({})|(\{})|({})",
-            simple_assign_op, add_assign_op, sub_assign_op
-        );
-        let newline_ptn = r"[\r\n]";
-        let wsp_ptn = r"[ \t]";
-        let decimal_number_ptn = r"[0-9]+";
-        let hexadecimal_number_ptn = r"(?i)0x[0-9A-F]+";
-        let number_ptn = &format!("({})|({})", hexadecimal_number_ptn, decimal_number_ptn);
-        let var_prefix = "v";
-        let static_variable_ptn = &format!("{}({})", var_prefix, number_ptn);
-        let dynamic_variable_ptn = &format!("{}{}", var_prefix, head_kw);
+        let wsp_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"[ \t]").unwrap());
+            LazyLock::force(&RE)
+        };
+        let newline_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"[\r\n]").unwrap());
+            LazyLock::force(&RE)
+        };
+        let semicolon_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(SEMICOLON).unwrap());
+            LazyLock::force(&RE)
+        };
+        let put_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(PUT_KW).unwrap());
+            LazyLock::force(&RE)
+        };
+        let get_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(GET_KW).unwrap());
+            LazyLock::force(&RE)
+        };
+        let while_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(WHILE_KW).unwrap());
+            LazyLock::force(&RE)
+        };
+        let head_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(HEAD_KW).unwrap());
+            LazyLock::force(&RE)
+        };
+        let bracket_re = {
+            static RE: LazyLock<Regex> =
+                LazyLock::new(|| Regex::new(&format!(r"[\{}\{}]", LBRACKET, RBRACKET)).unwrap());
+            LazyLock::force(&RE)
+        };
+        let curly_bracket_re = {
+            static RE: LazyLock<Regex> =
+                LazyLock::new(|| Regex::new(&format!(r"[\{}\{}]", LCBRACKET, RCBRACKET)).unwrap());
+            LazyLock::force(&RE)
+        };
+        let assign_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| {
+                Regex::new(&format!(
+                    r"({})|(\{})|({})",
+                    SIMPLE_ASSIGN_OP, ADD_ASSIGN_OP, SUB_ASSIGN_OP
+                ))
+                .unwrap()
+            });
+            LazyLock::force(&RE)
+        };
+        let number_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(NUMBER_PTN).unwrap());
+            LazyLock::force(&RE)
+        };
+        let static_variable_re = {
+            static RE: LazyLock<Regex> =
+                LazyLock::new(|| Regex::new(&format!("{}({})", VAR_PREFIX, NUMBER_PTN)).unwrap());
+            LazyLock::force(&RE)
+        };
+        let dynamic_variable_re = {
+            static RE: LazyLock<Regex> =
+                LazyLock::new(|| Regex::new(&format!("{}{}", VAR_PREFIX, HEAD_KW)).unwrap());
+            LazyLock::force(&RE)
+        };
 
-        let wsp_re = Regex::new(wsp_ptn).unwrap();
-        let newline_re = Regex::new(newline_ptn).unwrap();
-        let semicolon_re = Regex::new(semicolon).unwrap();
-        let put_re = Regex::new(put_kw).unwrap();
-        let get_re = Regex::new(get_kw).unwrap();
-        let while_re = Regex::new(while_kw).unwrap();
-        let head_re = Regex::new(head_kw).unwrap();
-        let bracket_re = Regex::new(bracket_ptn).unwrap();
-        let curly_bracket_re = Regex::new(curly_bracket_ptn).unwrap();
-        let assign_re = Regex::new(assign_ptn).unwrap();
-        let number_re = Regex::new(number_ptn).unwrap();
-        let static_variable_re = Regex::new(static_variable_ptn).unwrap();
-        let dynamic_variable_re = Regex::new(dynamic_variable_ptn).unwrap();
-        
-        let delimiter_re = Regex::new(r"[ \t\r\n;\(\)\{\}]+").unwrap();
+        let delimiter_re = {
+            static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(DELIMITER_PTN).unwrap());
+            LazyLock::force(&RE)
+        };
 
         let mut result = Vec::new();
         let mut cur: usize = 0;
         while cur < code.len() {
-            if let Some(_) = Self::cut_token(&wsp_re, code, &mut cur) {
+            if let Some(_) = Self::cut_token(wsp_re, code, &mut cur) {
                 // nothing to do
-            } else if let Some(_) = Self::cut_token(&newline_re, code, &mut cur) {
+            } else if let Some(_) = Self::cut_token(newline_re, code, &mut cur) {
                 result.push(Token::NewLine);
-            } else if let Some(_) = Self::cut_token(&semicolon_re, code, &mut cur) {
+            } else if let Some(_) = Self::cut_token(semicolon_re, code, &mut cur) {
                 result.push(Token::Semicolon);
-            } else if let Some(_) = Self::cut_token(&put_re, code, &mut cur) {
+            } else if let Some(_) = Self::cut_token(put_re, code, &mut cur) {
                 result.push(Token::Put);
-            } else if let Some(_) = Self::cut_token(&get_re, code, &mut cur) {
+            } else if let Some(_) = Self::cut_token(get_re, code, &mut cur) {
                 result.push(Token::Get)
-            } else if let Some(_) = Self::cut_token(&while_re, code, &mut cur) {
+            } else if let Some(_) = Self::cut_token(while_re, code, &mut cur) {
                 result.push(Token::While);
-            } else if let Some(_) = Self::cut_token(&head_re, code, &mut cur) {
+            } else if let Some(_) = Self::cut_token(head_re, code, &mut cur) {
                 result.push(Token::Head);
-            } else if let Some(bracket_str) = Self::cut_token(&bracket_re, code, &mut cur) {
+            } else if let Some(bracket_str) = Self::cut_token(bracket_re, code, &mut cur) {
                 result.push(Token::Bracket(match bracket_str {
-                    s if s == lbracket => BracketToken::Open,
-                    s if s == rbracket => BracketToken::Close,
+                    s if s == LBRACKET => BracketToken::Open,
+                    s if s == RBRACKET => BracketToken::Close,
                     _ => unreachable!("Unexpected bracket_re match"),
                 }));
-            } else if let Some(cbracket_str) = Self::cut_token(&curly_bracket_re, code, &mut cur) {
+            } else if let Some(cbracket_str) = Self::cut_token(curly_bracket_re, code, &mut cur) {
                 result.push(Token::CurlyBracket(match cbracket_str {
-                    s if s == lcbracket => CurlyBracketToken::Open,
-                    s if s == rcbracket => CurlyBracketToken::Close,
+                    s if s == LCBRACKET => CurlyBracketToken::Open,
+                    s if s == RCBRACKET => CurlyBracketToken::Close,
                     _ => unreachable!("Unexpected curly_bracket_re match"),
                 }));
-            } else if let Some(assign_str) = Self::cut_token(&assign_re, code, &mut cur) {
+            } else if let Some(assign_str) = Self::cut_token(assign_re, code, &mut cur) {
                 result.push(Token::Assign(match assign_str {
-                    s if s == simple_assign_op => AssginToken::Simple,
-                    s if s == add_assign_op => AssginToken::Add,
-                    s if s == sub_assign_op => AssginToken::Sub,
+                    s if s == SIMPLE_ASSIGN_OP => AssginToken::Simple,
+                    s if s == ADD_ASSIGN_OP => AssginToken::Add,
+                    s if s == SUB_ASSIGN_OP => AssginToken::Sub,
                     _ => unreachable!("Unexpected assign_re match"),
                 }));
-            } else if let Some(num_str) = Self::cut_token(&number_re, code, &mut cur) {
+            } else if let Some(num_str) = Self::cut_token(number_re, code, &mut cur) {
                 result.push(Token::Number {
                     value: Self::parse_number(num_str),
                 });
-            } else if let Some(stat_var_str) = Self::cut_token(&static_variable_re, code, &mut cur) {
-                let index = Self::parse_number(&stat_var_str[var_prefix.len()..]);
+            } else if let Some(stat_var_str) = Self::cut_token(static_variable_re, code, &mut cur) {
+                let index = Self::parse_number(&stat_var_str[VAR_PREFIX.len()..]);
                 result.push(Token::Variable(VariableToken::Static { index }));
-            } else if let Some(_) = Self::cut_token(&dynamic_variable_re, code, &mut cur) {
+            } else if let Some(_) = Self::cut_token(dynamic_variable_re, code, &mut cur) {
                 result.push(Token::Variable(VariableToken::Dynamic));
             } else {
                 let token_like = delimiter_re.splitn(&code[cur..], 2).next().unwrap();
